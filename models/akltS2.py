ae1a4c7923cf7819b1c5f8f41627614d017ae147
Juraj Hasik <jhasik@sissa.it>
2019-09-01 12:39:52 +0200

    ctmrg test for c4v aklt s2

--- models/akltS2.py
+++ models/akltS2.py
@@ -1,10 +1,15 @@
 import torch
 import su2
+from c4v import *
 import config as cfg
 import ipeps
 from ctm.generic.env import ENV
 from ctm.generic import rdm
-from args import GLOBALARGS
+from ctm.one_site_c4v.env_c4v import ENV_C4V
+from ctm.one_site_c4v import rdm_c4v
+from ctm.one_site_c4v import corrf_c4v
+from math import sqrt
+import itertools
 
 class AKLTS2():
     def __init__(self, global_args=cfg.global_args):
@@ -114,4 +119,165 @@ class AKLTS2():
                 for label,op in self.obs_ops.items():
                     obs[str(coord)+"|"+label] = torch.trace(rdm1x1@op)
 
-        return obs
+
+        obs= dict({"avg_m": 0.})
+        with torch.no_grad():
+            for coord,site in state.sites.items():
+                rdm1x1 = rdm.rdm1x1(coord,state,env)
+                for label,op in self.obs_ops.items():
+                    obs[f"{label}{coord}"]= torch.trace(rdm1x1@op)
+                obs[f"m{coord}"]= sqrt(abs(obs[f"sz{coord}"]**2 + obs[f"sp{coord}"]*obs[f"sm{coord}"]))
+                obs["avg_m"] += obs[f"m{coord}"]
+            obs["avg_m"]= obs["avg_m"]/len(state.sites.keys())
+
+            for coord,site in state.sites.items():
+                rdm2x1 = rdm.rdm2x1(coord,state,env)
+                rdm1x2 = rdm.rdm1x2(coord,state,env)
+                obs[f"SS2x1{coord}"]= torch.einsum('ijab,ijab',rdm2x1,self.h2)
+                obs[f"SS1x2{coord}"]= torch.einsum('ijab,ijab',rdm1x2,self.h2)
+        
+        # prepare list with labels and values
+        obs_labels=["avg_m"]+[f"m{coord}" for coord in state.sites.keys()]\
+            +[f"{lc[1]}{lc[0]}" for lc in list(itertools.product(state.sites.keys(), self.obs_ops.keys()))]
+        obs_labels += [f"SS2x1{coord}" for coord in state.sites.keys()]
+        obs_labels += [f"SS1x2{coord}" for coord in state.sites.keys()]
+        obs_values=[obs[label] for label in obs_labels]
+        return obs_values, obs_labels
+
+class AKLTS2_C4V_BIPARTITE():
+    def __init__(self, global_args=cfg.global_args):
+        self.dtype=global_args.dtype
+        self.device=global_args.device
+        self.phys_dim = 5
+
+        self.h2_rot, self.SS, self.SS_rot = self.get_h()
+        self.obs_ops = self.get_obs()
+
+    # build AKLT S=2 Hamiltonian <=> Projector from product of two S=2 DOFs
+    # to S=4 DOF H = \sum_{<i,j>} h_ij, where h_ij= ...
+    #
+    # indices of h correspond to s_i,s_j;s_i',s_j'
+    def get_h(self):
+        pd = self.phys_dim
+        s5 = su2.SU2(pd, dtype=self.dtype, device=self.device)
+        expr_kron = 'ij,ab->iajb'
+        SS = torch.einsum(expr_kron,s5.SZ(),s5.SZ()) + 0.5*(torch.einsum(expr_kron,s5.SP(),s5.SM()) \
+            + torch.einsum(expr_kron,s5.SM(),s5.SP()))
+        rot_op = su2.get_rot_op(5)
+        SS_rot = torch.einsum('jl,ilak,kb->ijab',rot_op,SS,rot_op)
+        SS = SS.view(pd*pd,pd*pd)
+        h = (1./14)*(SS + (7./10.)*SS@SS + (7./45.)*SS@SS@SS + (1./90.)*SS@SS@SS@SS)
+        h = h.view(pd,pd,pd,pd)
+        # apply a rotation on physical index of every "odd" site
+        # A A => A B
+        # A A => B A
+        h_rot = torch.einsum('jl,ilak,kb->ijab',rot_op,h,rot_op)
+        SS = SS.view(pd,pd,pd,pd)
+        return h_rot, SS, SS_rot
+
+    def get_obs(self):
+        obs_ops = dict()
+        s5 = su2.SU2(self.phys_dim, dtype=self.dtype, device=self.device)
+        obs_ops["sz"]= s5.SZ()
+        obs_ops["sp"]= s5.SP()
+        obs_ops["sm"]= s5.SM()
+        return obs_ops
+
+    def energy_1x1(self,state,env_c4v):
+        rdm2x1 = rdm_c4v.rdm2x1(state, env_c4v)
+        energy = torch.einsum('ijab,ijab',rdm2x1,self.h2_rot)
+        return energy
+        
+    # definition of other observables
+    def eval_obs(self,state,env_c4v):
+        r"""
+        :param state: wavefunction
+        :param env_c4v: CTM c4v symmetric environment
+        :type state: IPEPS
+        :type env_c4v: ENV_C4V
+        :return:  expectation values of observables, labels of observables
+        :rtype: list[float], list[str]
+
+        Computes the following observables in order
+
+            1. magnetization
+            2. :math:`\langle S^z \rangle,\ \langle S^+ \rangle,\ \langle S^- \rangle`
+    
+        where the on-site magnetization is defined as
+        
+        .. math::
+            
+            \begin{align*}
+            m &= \sqrt{ \langle S^z \rangle^2+\langle S^x \rangle^2+\langle S^y \rangle^2 }
+            =\sqrt{\langle S^z \rangle^2+1/4(\langle S^+ \rangle+\langle S^- 
+            \rangle)^2 -1/4(\langle S^+\rangle-\langle S^-\rangle)^2} \\
+              &=\sqrt{\langle S^z \rangle^2 + 1/2\langle S^+ \rangle \langle S^- \rangle)}
+            \end{align*}
+
+        Usual spin components can be obtained through the following relations
+        
+        .. math::
+            
+            \begin{align*}
+            S^+ &=S^x+iS^y               & S^x &= 1/2(S^+ + S^-)\\
+            S^- &=S^x-iS^y\ \Rightarrow\ & S^y &=-i/2(S^+ - S^-)
+            \end{align*}
+        """
+        # TODO optimize/unify ?
+        # expect "list" of (observable label, value) pairs ?
+        obs= dict()
+        with torch.no_grad():
+            # symmetrize on-site tensor
+            symm_sites= {(0,0): make_c4v_symm(state.sites[(0,0)])}
+            symm_sites[(0,0)]= symm_sites[(0,0)]/torch.max(torch.abs(symm_sites[(0,0)]))
+            symm_state= ipeps.IPEPS(symm_sites)
+
+            rdm1x1= rdm_c4v.rdm1x1(symm_state,env_c4v)
+            for label,op in self.obs_ops.items():
+                obs[f"{label}"]= torch.trace(rdm1x1@op)
+            obs[f"m"]= sqrt(abs(obs[f"sz"]**2 + obs[f"sp"]*obs[f"sm"]))
+            
+            rdm2x1 = rdm_c4v.rdm2x1(symm_state,env_c4v)
+            obs[f"SS2x1"]= torch.einsum('ijab,ijab',rdm2x1,self.SS_rot)
+            
+        # prepare list with labels and values
+        obs_labels=[f"m"]+[f"{lc}" for lc in self.obs_ops.keys()]+[f"SS2x1"]
+        obs_values=[obs[label] for label in obs_labels]
+        return obs_values, obs_labels
+
+    def eval_corrf_SS(self,state,env_c4v,dist):
+   
+        # function generating properly rotated operators on every bi-partite site
+        def get_bilat_op(op):
+            rot_op= su2.get_rot_op(self.phys_dim, dtype=self.dtype, device=self.device)
+            op_0= op
+            op_rot= torch.einsum('ki,kl,lj->ij',rot_op,op_0,rot_op)
+            def _gen_op(r):
+                return op_rot if r%2==0 else op_0
+            return _gen_op
+
+        op_sx= 0.5*(self.obs_ops["sp"] + self.obs_ops["sm"])
+        op_isy= -0.5*(self.obs_ops["sp"] - self.obs_ops["sm"]) 
+
+        Sz0szR= corrf_c4v.corrf_1sO1sO(state, env_c4v, self.obs_ops["sz"], \
+            get_bilat_op(self.obs_ops["sz"]), dist)
+        Sx0sxR= corrf_c4v.corrf_1sO1sO(state, env_c4v, op_sx, get_bilat_op(op_sx), dist)
+        nSy0SyR= corrf_c4v.corrf_1sO1sO(state, env_c4v, op_isy, get_bilat_op(op_isy), dist)
+
+        res= dict({"ss": Sz0szR+Sx0sxR-nSy0SyR, "szsz": Sz0szR, "sxsx": Sx0sxR, "sysy": -nSy0SyR})
+        return res
+
+    def eval_corrf_DD_H(self,state,env_c4v,dist,verbosity=0):
+        # function generating properly rotated S.S operator on every bi-partite site
+        rot_op= su2.get_rot_op(self.phys_dim, dtype=self.dtype, device=self.device)
+        # (S.S)_s1s2,s1's2' with rotation applied on "first" spin s1,s1' 
+        SS_rot= torch.einsum('ki,kjcb,ca->ijab',rot_op,self.SS,rot_op)
+        # (S.S)_s1s2,s1's2' with rotation applied on "second" spin s2,s2'
+        op_rot= SS_rot.permute(1,0,3,2).contiguous()
+        def _gen_op(r):
+            return SS_rot if r%2==0 else op_rot
+        
+        D0DR= corrf_c4v.corrf_2sO2sO_H(state, env_c4v, SS_rot, _gen_op, dist, verbosity=verbosity)
+
+        res= dict({"dd": D0DR})
+        return res
---
+++ tests/test_ctmrg_j1j2.py
@@ -0,0 +1,114 @@
+import context
+import torch
+import argparse
+import config as cfg
+from ipeps import *
+from ctm.generic.env import *
+from ctm.generic import ctmrg
+from models import j1j2
+
+if __name__=='__main__':
+    # parse command line args and build necessary configuration objects
+    parser= cfg.get_args_parser()
+    # additional model-dependent arguments
+    parser.add_argument("-j1", type=float, default=1., help="nearest-neighbour coupling")
+    parser.add_argument("-j2", type=float, default=0., help="next nearest-neighbour coupling")
+    parser.add_argument("-tiling", default="BIPARTITE", help="tiling of the lattice")
+    # additional observables-related arguments
+    parser.add_argument("-corrf_r", type=int, default=1, help="maximal correlation function distance")
+    args = parser.parse_args()
+    cfg.configure(args)
+    cfg.print_config()
+    torch.set_num_threads(args.omp_cores)
+    
+    model = j1j2.J1J2(j1=args.j1, j2=args.j2)
+    
+    # initialize an ipeps
+    # 1) define lattice-tiling function, that maps arbitrary vertex of square lattice
+    # coord into one of coordinates within unit-cell of iPEPS ansatz    
+    if args.tiling == "BIPARTITE":
+        def lattice_to_site(coord):
+            vx = (coord[0] + abs(coord[0]) * 2) % 2
+            vy = abs(coord[1])
+            return ((vx + vy) % 2, 0)
+    elif args.tiling == "2SITE":
+        def lattice_to_site(coord):
+            vx = (coord[0] + abs(coord[0]) * 2) % 2
+            vy = (coord[1] + abs(coord[1]) * 1) % 1
+            return (vx, vy)
+    elif args.tiling == "4SITE":
+        def lattice_to_site(coord):
+            vx = (coord[0] + abs(coord[0]) * 2) % 2
+            vy = (coord[1] + abs(coord[1]) * 2) % 2
+            return (vx, vy)
+    else:
+        raise ValueError("Invalid tiling: "+str(args.tiling)+" Supported options: "\
+            +"BIPARTITE, 2SITE, 4SITE")
+
+    # initialize an ipeps
+    if args.instate!=None:
+        state = read_ipeps(args.instate, vertexToSite=lattice_to_site)
+        if args.bond_dim > max(state.get_aux_bond_dims()):
+            # extend the auxiliary dimensions
+            state = extend_bond_dim(state, args.bond_dim)
+        add_random_noise(state, args.instate_noise)
+    elif args.ipeps_init_type=='RANDOM':
+        bond_dim = args.bond_dim
+        
+        A = torch.rand((model.phys_dim, bond_dim, bond_dim, bond_dim, bond_dim),\
+            dtype=cfg.global_args.dtype)
+        B = torch.rand((model.phys_dim, bond_dim, bond_dim, bond_dim, bond_dim),\
+            dtype=cfg.global_args.dtype)
+
+        # normalization of initial random tensors
+        A = A/torch.max(torch.abs(A))
+        B = B/torch.max(torch.abs(B))
+
+        sites = {(0,0): A, (1,0): B}
+        
+        if args.tiling == "4SITE":
+            C= torch.rand((model.phys_dim, bond_dim, bond_dim, bond_dim, bond_dim),\
+                dtype=cfg.global_args.dtype)
+            D= torch.rand((model.phys_dim, bond_dim, bond_dim, bond_dim, bond_dim),\
+                dtype=cfg.global_args.dtype)
+            sites[(0,1)]= C/torch.max(torch.abs(C))
+            sites[(1,1)] = D/torch.max(torch.abs(D))
+
+        state = IPEPS(sites, vertexToSite=lattice_to_site)
+    else:
+        raise ValueError("Missing trial state: -instate=None and -ipeps_init_type= "\
+            +str(args.ipeps_init_type)+" is not supported")
+
+    print(state)
+
+    # 2) select the "energy" function 
+    if args.tiling == "BIPARTITE" or args.tiling == "2SITE":
+        energy_f=model.energy_2x2_2site
+    elif args.tiling == "4SITE":
+        energy_f=model.energy_2x2_4site
+    else:
+        raise ValueError("Invalid tiling: "+str(args.tiling)+" Supported options: "\
+            +"BIPARTITE, 2SITE, 4SITE")
+
+    def ctmrg_conv_energy(state, env, history, ctm_args=cfg.ctm_args):
+        with torch.no_grad():
+            e_curr = energy_f(state, env)
+            obs_values, obs_labels = model.eval_obs(state, env)
+            history.append([e_curr.item()]+obs_values)
+            print(", ".join([f"{len(history)}",f"{e_curr}"]+[f"{v}" for v in obs_values]))
+
+            if len(history) > 1 and abs(history[-1][0]-history[-2][0]) < ctm_args.ctm_conv_tol:
+                return True
+        return False
+
+    ctm_env_init = ENV(args.chi, state)
+    init_env(state, ctm_env_init)
+    print(ctm_env_init)
+
+    e_curr0 = energy_f(state, ctm_env_init)
+    obs_values0, obs_labels = model.eval_obs(state,ctm_env_init)
+
+    print(", ".join(["epoch","energy"]+obs_labels))
+    print(", ".join([f"{-1}",f"{e_curr0}"]+[f"{v}" for v in obs_values0]))
+
+    ctm_env_init, history, t_ctm = ctmrg.run(state, ctm_env_init, conv_check=ctmrg_conv_energy)
