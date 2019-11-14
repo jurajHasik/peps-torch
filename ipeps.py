3e3b00804529fe5e7338327f0e26b119304a6c39
Juraj Hasik <jhasik@sissa.it>
2019-11-14 21:58:59 +0100

    testing implementation of su2-sym ansatz

--- ipeps.py
+++ ipeps.py
@@ -4,7 +4,7 @@ import itertools
 import math
 import config as cfg
 
-class IPEPS(torch.nn.Module):
+class IPEPS():
     def __init__(self, sites, vertexToSite=None, lX=None, lY=None, peps_args=cfg.peps_args,\
         global_args=cfg.global_args):
         r"""
@@ -16,6 +16,152 @@ class IPEPS(torch.nn.Module):
         :param peps_args: ipeps configuration
         :param global_args: global configuration
         :type sites: dict[tuple(int,int) : torch.tensor]
+        :type vertexToSite: function(tuple(int,int))->tuple(int,int)
+        :type lX: int
+        :type lY: int
+        :type peps_args: PEPSARGS
+        :type global_args: GLOBALARGS
+        Member ``sites`` is a dictionary of non-equivalent on-site tensors
+        indexed by tuple of coordinates (x,y) within the elementary unit cell.
+        The index-position convetion for on-site tensors is defined as follows::
+               u s 
+               |/ 
+            l--A--r  <=> A[s,u,l,d,r]
+               |
+               d
+            
+            where s denotes physical index, and u,l,d,r label four principal directions
+            up, left, down, right in anti-clockwise order starting from up
+        Member ``vertexToSite`` is a mapping function from vertex on a square lattice
+        passed in as tuple(x,y) to a corresponding tuple(x,y) within elementary unit cell.
+        
+        On-site tensor of an IPEPS object ``wfc`` at vertex (x,y) is conveniently accessed 
+        through the member function ``site``, which internally uses ``vertexToSite`` mapping::
+            
+            coord= (0,0)
+            A_00= wfc.site(coord)  
+        By combining the appropriate ``vertexToSite`` mapping function with elementary unit 
+        cell specified through ``sites`` various tilings of a square lattice can be achieved:: 
+            # Example 1: 1-site translational iPEPS
+            
+            sites={(0,0): A}
+            def vertexToSite(coord):
+                return (0,0)
+            wfc= IPEPS(sites,vertexToSite)
+        
+            # resulting tiling:
+            # y\x -2 -1 0 1 2
+            # -2   A  A A A A
+            # -1   A  A A A A
+            #  0   A  A A A A
+            #  1   A  A A A A
+            # Example 2: 2-site bipartite iPEPS
+            
+            sites={(0,0): A, (1,0): B}
+            def vertexToSite(coord):
+                x = (coord[0] + abs(coord[0]) * 2) % 2
+                y = abs(coord[1])
+                return ((x + y) % 2, 0)
+            wfc= IPEPS(sites,vertexToSite)
+        
+            # resulting tiling:
+            # y\x -2 -1 0 1 2
+            # -2   A  B A B A
+            # -1   B  A B A B
+            #  0   A  B A B A
+            #  1   B  A B A B
+        
+            # Example 3: iPEPS with 3x2 unit cell with PBC 
+            
+            sites={(0,0): A, (1,0): B, (2,0): C, (0,1): D, (1,1): E, (2,1): F}
+            wfc= IPEPS(sites,lX=3,lY=2)
+            
+            # resulting tiling:
+            # y\x -2 -1 0 1 2
+            # -2   B  C A B C
+            # -1   E  F D E F
+            #  0   B  C A B C
+            #  1   E  F D E F
+        where in the last example we used default setting for ``vertexToSite``, which
+        maps square lattice into elementary unit cell of size `lX` x `lY` assuming 
+        periodic boundary conditions (PBC) along both X and Y directions.
+        TODO we infer the size of the cluster from the keys of sites. Is it OK?
+        """
+        super(IPEPS, self).__init__()
+        self.dtype = global_args.dtype
+        self.device = global_args.device
+
+        self.sites = sites
+        
+        # infer the size of the cluster
+        if lX is None or lY is None:
+            min_x = min([coord[0] for coord in sites.keys()])
+            max_x = max([coord[0] for coord in sites.keys()])
+            min_y = min([coord[1] for coord in sites.keys()])
+            max_y = max([coord[1] for coord in sites.keys()])
+            self.lX = max_x-min_x + 1
+            self.lY = max_y-min_y + 1
+        else:
+            self.lX = lX
+            self.lY = lY
+
+        if vertexToSite is not None:
+            self.vertexToSite = vertexToSite
+        else:
+            def vertexToSite(coord):
+                x = coord[0]
+                y = coord[1]
+                return ( (x + abs(x)*self.lX)%self.lX, (y + abs(y)*self.lY)%self.lY )
+            self.vertexToSite = vertexToSite
+
+    def __str__(self):
+        print(f"lX x lY: {self.lX} x {self.lY}")
+        for nid,coord,site in [(t[0], *t[1]) for t in enumerate(self.coeffs.items())]:
+            print(f"A{nid} {coord}: {site.size()}")
+        
+        # show tiling of a square lattice
+        coord_list = list(self.coeffs.keys())
+        mx, my = 3*self.lX, 3*self.lY
+        label_spacing = 1+int(math.log10(len(self.coeffs.keys())))
+        for y in range(-my,my):
+            if y == -my:
+                print("y\\x ", end="")
+                for x in range(-mx,mx):
+                    print(str(x)+label_spacing*" "+" ", end="")
+                print("")
+            print(f"{y:+} ", end="")
+            for x in range(-mx,mx):
+                print(f"A{coord_list.index(self.vertexToSite((x,y)))} ", end="")
+            print("")
+        
+        return ""
+
+    def site(self, coord):
+        """
+        :param coord: tuple (x,y) specifying vertex on a square lattice
+        :type coord: tuple(int,int)
+        :return: on-site tensor corresponding to the vertex (x,y)
+        :rtype: torch.tensor
+        """
+        return self.sites[self.vertexToSite(coord)]
+
+    def get_aux_bond_dims(self):
+        return [d for key in self.sites.keys() for d in self.sites[key].size()[1:]]
+
+class IPEPS_SU2SYM(IPEPS):
+    def __init__(self, su2_tensors, coeffs, vertexToSite=None, lX=None, lY=None, \
+        peps_args=cfg.peps_args, global_args=cfg.global_args):
+        r"""
+        :param su2_tensors: list of selected SU(2)-symmetric tensors
+        :param coeffs: map from elementary unit cell to vector of coefficients
+        :param vertexToSite: function mapping arbitrary vertex of a square lattice 
+                             into a vertex within elementary unit cell
+        :param lX: length of the elementary unit cell in X direction
+        :param lY: length of the elementary unit cell in Y direction
+        :param peps_args: ipeps configuration
+        :param global_args: global configuration
+        :type su2_tensors: list[tuple(dict(str,str), torch.tensor)]
+        :type coeffs: dict[tuple(int,int) : torch.tensor]
         :type vertexToSite: function(tuple(int,int))->tuple(int,int)
         :type lX: int
         :type lY: int
@@ -95,69 +241,22 @@ class IPEPS(torch.nn.Module):
 
         TODO we infer the size of the cluster from the keys of sites. Is it OK?
         """
-        super(IPEPS, self).__init__()
-        self.dtype = global_args.dtype
-        self.device = global_args.device
+        self.su2_tensors= su2_tensors
+        self.coeffs= coeffs
+        sites= self.build_onsite_tensors()
 
-        self.sites = sites
-        
-        # infer the size of the cluster
-        if lX is None or lY is None:
-            min_x = min([coord[0] for coord in sites.keys()])
-            max_x = max([coord[0] for coord in sites.keys()])
-            min_y = min([coord[1] for coord in sites.keys()])
-            max_y = max([coord[1] for coord in sites.keys()])
-            self.lX = max_x-min_x + 1
-            self.lY = max_y-min_y + 1
-        else:
-            self.lX = lX
-            self.lY = lY
+        super().__init__(sites, vertexToSite=vertexToSite, peps_args=peps_args,\
+            global_args=global_args)
 
-        if vertexToSite is not None:
-            self.vertexToSite = vertexToSite
-        else:
-            def vertexToSite(coord):
-                x = coord[0]
-                y = coord[1]
-                return ( (x + abs(x)*self.lX)%self.lX, (y + abs(y)*self.lY)%self.lY )
-            self.vertexToSite = vertexToSite
-
-    def __str__(self):
-        print(f"lX x lY: {self.lX} x {self.lY}")
-        for nid,coord,site in [(t[0], *t[1]) for t in enumerate(self.sites.items())]:
-            print(f"A{nid} {coord}: {site.size()}")
-        
-        # show tiling of a square lattice
-        coord_list = list(self.sites.keys())
-        mx, my = 3*self.lX, 3*self.lY
-        label_spacing = 1+int(math.log10(len(self.sites.keys())))
-        for y in range(-my,my):
-            if y == -my:
-                print("y\\x ", end="")
-                for x in range(-mx,mx):
-                    print(str(x)+label_spacing*" "+" ", end="")
-                print("")
-            print(f"{y:+} ", end="")
-            for x in range(-mx,mx):
-                print(f"A{coord_list.index(self.vertexToSite((x,y)))} ", end="")
-            print("")
-        
-        return ""
-
-    def site(self, coord):
-        """
-        :param coord: tuple (x,y) specifying vertex on a square lattice
-        :type coord: tuple(int,int)
-        :return: on-site tensor corresponding to the vertex (x,y)
-        :rtype: torch.tensor
-        """
-        return self.sites[self.vertexToSite(coord)]
-
-    def get_aux_bond_dims(self):
-        return [d for key in self.sites.keys() for d in self.sites[key].size()[1:]]
+    def build_onsite_tensors(self):
+        ts= torch.stack([t for m,t in self.su2_tensors])
+        sites=dict()
+        for coord,c in self.coeffs.items():
+            sites[coord]= torch.einsum('i,ipuldr->puldr',c,ts) 
+        return sites
 
 def read_ipeps(jsonfile, vertexToSite=None, aux_seq=[0,1,2,3], peps_args=cfg.peps_args,\
-        global_args=cfg.global_args):
+    global_args=cfg.global_args):
     r"""
     :param jsonfile: input file describing iPEPS in json format
     :param vertexToSite: function mapping arbitrary vertex of a square lattice 
@@ -294,7 +393,7 @@ def add_random_noise(state, noise=0.):
         rand_t = torch.rand( state.sites[coord].size(), dtype=state.dtype, device=state.device)
         state.sites[coord] = state.sites[coord] + noise * rand_t
 
-def write_ipeps(state, outputfile, aux_seq=[0,1,2,3], tol=1.0e-10, normalize=False):
+def write_ipeps(state, outputfile, aux_seq=[0,1,2,3], tol=1.0e-14, normalize=False):
     r"""
     :param state: wavefunction to write out in json format
     :param outputfile: target file
@@ -327,34 +426,51 @@ def write_ipeps(state, outputfile, aux_seq=[0,1,2,3], tol=1.0e-10, normalize=Fal
     TODO implement cutoff on elements with magnitude below tol
     """
     asq = [x+1 for x in aux_seq]
-    json_state=dict({"lX": state.lX, "lY": state.lY, "sites": []})
+    json_state=dict({"lX": state.lX, "lY": state.lY, "su2_tensors": [], "coeffs": []})
     
+    # write list of considered SU(2)-symmetric tensors
+    for meta,t in state.su2_tensors:
+        json_tensor=dict()
+        json_tensor["meta"]=meta
+
+        tdims = t.size()
+        tlength = tdims[0]*tdims[1]*tdims[2]*tdims[3]*tdims[4]
+        json_tensor["physDim"]= tdims[0]
+        # assuming all auxBondDim are identical
+        json_tensor["auxDim"]= tdims[1]
+        # get non-zero elements
+        t_nonzero= t.nonzero()
+        json_tensor["numEntries"]= len(t_nonzero)
+        entries = []
+        for elem in t_nonzero:
+            ei=tuple(elem.tolist())
+            entries.append(f"{ei[0]} {ei[asq[0]]} {ei[asq[1]]} {ei[asq[2]]} {ei[asq[3]]}"\
+                +f" {t[ei]}")
+        json_tensor["entries"]=entries
+        json_state["su2_tensors"].append(json_tensor)
+
     site_ids=[]
     site_map=[]
-    for nid,coord,site in [(t[0], *t[1]) for t in enumerate(state.sites.items())]:
+    for nid,coord,c in [(t[0], *t[1]) for t in enumerate(state.coeffs.items())]:
         if normalize:
-            site= site/torch.max(torch.abs(site))
+            c= c/torch.max(torch.abs(c))
 
         json_tensor=dict()
         
-        tdims = site.size()
-        tlength = tdims[0]*tdims[1]*tdims[2]*tdims[3]*tdims[4]
+        tdims = c.size()
+        tlength = tdims[0]
         
         site_ids.append(f"A{nid}")
         site_map.append(dict({"siteId": site_ids[-1], "x": coord[0], "y": coord[1]} ))
         json_tensor["siteId"]=site_ids[-1]
-        json_tensor["physDim"]= tdims[0]
         # assuming all auxBondDim are identical
-        json_tensor["auxDim"]= tdims[1]
         json_tensor["numEntries"]= tlength
         entries = []
-        elem_inds = list(itertools.product( *(range(i) for i in tdims) ))
-        for ei in elem_inds:
-            entries.append(f"{ei[0]} {ei[asq[0]]} {ei[asq[1]]} {ei[asq[2]]} {ei[asq[3]]}"\
-                +f" {site[ei[0]][ei[1]][ei[2]][ei[3]][ei[4]]}")
+        for i in range(len(c)):
+            entries.append(f"{i} {c[i]}")
             
         json_tensor["entries"]=entries
-        json_state["sites"].append(json_tensor)
+        json_state["coeffs"].append(json_tensor)
 
     json_state["siteIds"]=site_ids
     json_state["map"]=site_map
