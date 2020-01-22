31a303283921f9743fa84da5d6c96b21a2dcbfc6
Juraj Hasik <juraj.hasik.sk@gmail.com>
2020-01-22 18:52:02 +0100

    faster rdm1x1 and 2x1 construction

--- ctm/one_site_c4v/rdm_c4v.py
+++ ctm/one_site_c4v/rdm_c4v.py
@@ -32,7 +32,7 @@ def rdm1x1(state, env, verbosity=0):
     # 0
     # T--2
     # 1
-    rdm = torch.tensordot(C,T,([0],[0]))
+    CTC = torch.tensordot(C,T,([0],[0]))
     if verbosity>2: env.log(f"rdm=CT {rdm.size()}\n")
     # C--0
     # |
@@ -40,14 +40,14 @@ def rdm1x1(state, env, verbosity=0):
     # 1
     # 0
     # C--1->2
-    rdm = torch.tensordot(rdm,C,([1],[0]))
+    CTC = torch.tensordot(CTC,C,([1],[0]))
     if verbosity>2: env.log(f"rdm=CTC {rdm.size()}\n")
     # C--0
     # |
     # T--1
     # |       2->3
     # C--2 0--T--1->2
-    rdm = torch.tensordot(rdm,T,([2],[0]))
+    rdm = torch.tensordot(CTC,T,([2],[0]))
     if verbosity>2: env.log(f"rdm=CTCT {rdm.size()}\n")
     # TODO - more efficent contraction with uncontracted-double-layer on-site tensor
     #        Possibly reshape indices 1,2 of rdm, which are to be contracted with 
@@ -83,32 +83,15 @@ def rdm1x1(state, env, verbosity=0):
     # C-------T--1
     rdm = torch.tensordot(T,rdm,([0,2],[0,2]))
     if verbosity>2: env.log(f"rdm=CTCTaT {rdm.size()}\n")
-    # C--T--0 0--C
-    # |  |       1->0
-    # |  |
-    # T--a--2
-    # |  |\34(s,s')
-    # |  |
-    # C--T--1
-    rdm = torch.tensordot(C,rdm,([0],[0]))
-    if verbosity>2: env.log(f"rdm=CTCTaTC {rdm.size()}\n")
-    # C--T---------------C
-    # |  |               0
-    # |  |               0
-    # T--a--2 2----------T
-    # |  |\34->23(s,s')  1->0
-    # |  |
-    # C--T--1
-    rdm = torch.tensordot(T,rdm,([0,2],[0,2]))
-    if verbosity>2: env.log(f"rdm=CTCTaTCT {rdm.size()}\n")
-    # C--T--------------C
+    
+    # C--T---0 0--------C
     # |  |              |
     # |  |              |
-    # T--a--------------T
-    # |  |\23->12(s,s') 0
-    # |  |              0
-    # C--T--1 1---------C
-    rdm = torch.tensordot(rdm,C,([0,1],[0,1]))
+    # T--a---2 1--------T
+    # |  |\34->01(s,s') |
+    # |  |              |
+    # C--T---1 2--------C
+    rdm = torch.tensordot(rdm,CTC,([0,1,2],[0,2,1]))
     if verbosity>2: env.log(f"rdm=CTCTaTCTC {rdm.size()}\n")
 
     # symmetrize
@@ -158,7 +141,7 @@ def rdm1x1_sl(state, env, verbosity=0):
     # 0
     # T--2
     # 1
-    rdm = torch.tensordot(C,T,([0],[0]))
+    CTC = torch.tensordot(C,T,([0],[0]))
     if verbosity>2: env.log(f"rdm=CT {rdm.size()}\n")
     # C--0
     # |
@@ -166,14 +149,14 @@ def rdm1x1_sl(state, env, verbosity=0):
     # 1
     # 0
     # C--1->2
-    rdm = torch.tensordot(rdm,C,([1],[0]))
+    CTC = torch.tensordot(CTC,C,([1],[0]))
     if verbosity>2: env.log(f"rdm=CTC {rdm.size()}\n")
     # C--0
     # |
     # T--1
     # |       2->3
     # C--2 0--T--1->2
-    rdm = torch.tensordot(rdm,T,([2],[0]))
+    rdm = torch.tensordot(CTC,T,([2],[0]))
     
     # 4i) untangle the fused D^2 indices
     #
@@ -241,32 +224,15 @@ def rdm1x1_sl(state, env, verbosity=0):
     # C-------T--1
     rdm = torch.tensordot(T,rdm,([0,2],[0,2]))
     if verbosity>2: env.log(f"rdm=CTCTaT {rdm.size()}\n")
-    # C--T--0 0--C
-    # |  |       1->0
-    # |  |
-    # T--a--2
-    # |  |\34(s,s')
-    # |  |
-    # C--T--1
-    rdm = torch.tensordot(C,rdm,([0],[0]))
-    if verbosity>2: env.log(f"rdm=CTCTaTC {rdm.size()}\n")
-    # C--T---------------C
-    # |  |               0
-    # |  |               0
-    # T--a--2 2----------T
-    # |  |\34->23(s,s')  1->0
-    # |  |
-    # C--T--1
-    rdm = torch.tensordot(T,rdm,([0,2],[0,2]))
-    if verbosity>2: env.log(f"rdm=CTCTaTCT {rdm.size()}\n")
-    # C--T--------------C
+
+    # C--T--0 0---------C
     # |  |              |
     # |  |              |
-    # T--a--------------T
-    # |  |\23->12(s,s') 0
-    # |  |              0
-    # C--T--1 1---------C
-    rdm = torch.tensordot(rdm,C,([0,1],[0,1]))
+    # T--a--2 1---------T
+    # |  |\34->01(s,s') |
+    # |  |              |
+    # C--T--1 2---------C
+    rdm = torch.tensordot(rdm,CTC,([0,1,2],[0,2,1]))
     if verbosity>2: env.log(f"rdm=CTCTaTCTC {rdm.size()}\n")
 
     # symmetrize
@@ -355,6 +321,111 @@ def rdm2x1(state, env, verbosity=0):
     C2x2 = C2x2.permute(1,2,0,3,4,5).contiguous().view(\
         T.size()[1],a.size()[3],T.size()[1]*a.size()[2],dimsA[0],dimsA[0])
     if verbosity>2: env.log(f"C2X2 {C2x2.size()}\n")
+
+    #----- build left part C2x2_LU--C2x1_LD ------------------------------------
+    # C2x2--2->1
+    # | |\34->23
+    # 0 1
+    # 0 2
+    # C2x1--1->0
+    left_half = torch.tensordot(C2x1, C2x2, ([0,2],[0,1]))
+
+    # construct reduced density matrix by contracting left and right halfs
+    # C2x2--1 1----C2x2
+    # |\23->01     |\23
+    # |            |
+    # C2x1--0 0----C2x1
+    rdm = torch.tensordot(left_half,left_half,([0,1],[0,1]))
+
+    # permute into order of s0,s1;s0',s1' where primed indices
+    # represent "ket"
+    # 0123->0213
+    rdm= rdm.permute(0,2,1,3).contiguous()
+
+    # symmetrize
+    dimsRDM= rdm.size()
+    rdm= rdm.view(dimsRDM[0]**2,dimsRDM[0]**2)
+    rdm= 0.5*(rdm+rdm.t())
+    # TODO make pos-def ?
+    eps=0.0
+    with torch.no_grad():
+        D, U= torch.eig(rdm)
+        # check only real part
+        if D[:,0].min() < 0:
+            eps= D[:,0].min().abs()
+    rdm+= eps*torch.eye(rdm.size()[0],dtype=rdm.dtype,device=rdm.device)
+
+    # normalize and reshape
+    if verbosity>0: env.log(f"Tr(rdm2x1): {torch.trace(rdm)}\n")
+    rdm= rdm / torch.trace(rdm)
+    rdm= rdm.view(dimsRDM)
+
+    return rdm
+
+def rdm2x1_sl(state, env, verbosity=0):
+    r"""
+    :param state: underlying 1-site C4v symmetric wavefunction
+    :param env: C4v symmetric environment corresponding to ``state``
+    :param verbosity: logging verbosity
+    :type state: IPEPS
+    :type env: ENV_C4V
+    :type verbosity: int
+    :return: 2-site reduced density matrix with indices :math:`s_0s_1;s'_0s'_1`
+    :rtype: torch.tensor
+
+    Computes 2-site reduced density matrix :math:`\rho_{2x1}` of a horizontal 
+    2x1 subsystem using following strategy:
+    
+        1. compute upper left 2x2 corner and lower left 2x1 corner
+        2. construct left half of the network
+        3. contract left and right half (identical to the left) to obtain final 
+           reduced density matrix
+
+    ::
+
+        C--T-----T-----C = C2x2--C2x2
+        |  |     |     |   |     |  
+        T--A^+A--A^+A--T   C2x1--C2x1
+        |  |     |     |
+        C--T-----T-----C 
+
+    The physical indices `s` and `s'` of on-sites tensors :math:`A` (and :math:`A^\dagger`) 
+    are left uncontracted and given in the following order::
+        
+        s0 s1
+
+    The resulting reduced density matrix is identical to the one of vertical 1x2 subsystem
+    due to the C4v symmetry. 
+    """
+    #----- building C2x2_LU ----------------------------------------------------
+    C = env.C[env.keyC]
+    T = env.T[env.keyT]
+    a = next(iter(state.sites.values()))
+
+    # C--1 0--T--1
+    # 0       2
+    C2x1 = torch.tensordot(C, T, ([1],[0]))
+
+    # see _get_open_C2x2_LU_sl
+    C2x2 = torch.tensordot(C2x1, T, ([0],[0]))
+    C2x2= C2x2.view(C2x2.size()[0],a.size()[1],a.size()[1],C2x2.size()[2],a.size()[2],\
+        a.size()[2])
+    C2x2= torch.tensordot(C2x2, a,([1,4],[1,2]))
+    C2x2= torch.tensordot(C2x2, a,([1,3],[1,2]))
+
+    # 4iv) fuse (some) pairs of aux indices
+    #
+    # C------T----0\
+    # | 3<-2\|\     \
+    # T------a----4\ \ 
+    # | \    | |    ->->2
+    # |  ------a--7/
+    # |      | |\5->4
+    # 1->0  (3 6)->1
+    # 
+    # permute and reshape 01234567->1(36)(047)25->01234
+    C2x2= C2x2.permute(1,3,6,0,4,7,2,5).contiguous().view(C2x2.size()[1],(a.size()[3]**2),\
+        C2x2.size()[0]*(a.size()[4]**2),a.size()[0],a.size()[0])
 
     #----- build left part C2x2_LU--C2x1_LD ------------------------------------
     # C2x2--2->1
@@ -911,17 +982,15 @@ def _get_open_C2x2_LU_sl(C, T, a, verbosity=0):
 
     # 4iv) fuse pairs of aux indices
     #
-    # C------T----0
-    # | 4<-2\|\
-    # T------a----4\ 
-    # | \    | |    ->3
-    # |  ------a--7/
-    # |      | |\5->3
-    # 1     (3 6)->2
+    #  C------T----0\
+    #  |    2\|\     \
+    #  T------a----4\ \ 
+    #  | \    | |    ->->1
+    #  |  ------a--7/
+    #  |      | |\5->3
+    # (1     (3 6))->0
     # 
-    # and simultaneously
-    # permute 0123->1203
-    # reshape (12)(03)45->0123
+    # permute and reshape 01234567->(136)(047)25->0123
     C2x2= C2x2.permute(1,3,6,0,4,7,2,5).contiguous().view(C2x2.size()[1]*(a.size()[3]**2),\
         C2x2.size()[0]*(a.size()[4]**2),a.size()[0],a.size()[0])
 
