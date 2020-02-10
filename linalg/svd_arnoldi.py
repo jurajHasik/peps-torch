cf12923d6779e22437a2231cbdfa274e5453500b
Juraj Hasik <juraj.hasik.sk@gmail.com>
2020-02-10 11:21:16 +0100

    bugfix arnoldi svd for non-sym matrices

--- linalg/svd_arnoldi.py
+++ linalg/svd_arnoldi.py
@@ -80,9 +80,6 @@ def test_SVDSYMARNOLDI_random():
     assert( torch.norm(M-U@torch.diag(S)@V.t())-torch.sqrt(torch.sum(S0[k:]**2)) 
         < S0[0]*(m**2)*1e-14 )
 
-if __name__=='__main__':
-    test_SVDSYMARNOLDI_random()
-
 class SVDARNOLDI(torch.autograd.Function):
     @staticmethod
     def forward(self, M, k):
@@ -104,29 +101,28 @@ class SVDARNOLDI(torch.autograd.Function):
         # input validation is provided by the scipy.sparse.linalg.eigsh
         
         # allow for mat-vec ops to be carried out on GPU
+        M_nograd = M.clone().detach()
+        MMt= M_nograd@M_nograd.t()
+        
         def mv(v):
             B= torch.as_tensor(v,dtype=M.dtype,device=M.device)
-            B= torch.mv(M,B)
-            return B.detach().cpu().numpy()
-        def vm(v):
-            B= torch.as_tensor(v,dtype=M.dtype,device=M.device)
-            B= torch.matmul(M.t(),B)            
+            B= torch.mv(MMt,B)
             return B.detach().cpu().numpy()
 
-        M_nograd= LinearOperator(M.size(), matvec=mv, rmatvec=vm)
+        MMt_op= LinearOperator(M.size(), matvec=mv)
 
-        U, S, V= scipy.sparse.linalg.svds(M_nograd, k=k)
+        D, U= scipy.sparse.linalg.eigsh(MMt_op, k=k)
+        D= torch.as_tensor(D,device=M.device)
+        U= torch.as_tensor(U,device=M.device)
 
-        S= torch.as_tensor(S)
-        U= torch.as_tensor(U)
-        # transpose wrt to pytorch
-        V= torch.as_tensor(V)
-        V= V.t()
+        # reorder the eigenpairs by the largest magnitude of eigenvalues
+        S,p= torch.sort(torch.abs(D),descending=True)
+        S= torch.sqrt(S)
+        U= U[:,p]
 
-        if M.is_cuda:
-            U= U.cuda()
-            V= V.cuda()
-            S= S.cuda()
+        # compute right singular vectors as Mt = V.S.Ut /.U => Mt.U = V.S
+        V = M_nograd.t() @ U
+        V = Functional.normalize(V, p=2, dim=0)
 
         self.save_for_backward(U, S, V)
         return U, S, V
@@ -136,3 +132,20 @@ class SVDARNOLDI(torch.autograd.Function):
         raise Exception("backward not implemented")
         U, S, V = self.saved_tensors
         return dA, None
+
+def test_SVDARNOLDI_random():
+    m= 50
+    k= 10
+    M= torch.rand(m, m, dtype=torch.float64)
+
+    U0, S0, V0= torch.svd(M)
+
+    U,S,V= SVDARNOLDI.apply(M,k)
+    # |M|=\sqrt{Tr(MM^t)}=\sqrt{Tr(D^2)} => 
+    # |M-US_kV^t|=\sqrt{Tr(D^2)-Tr(S^2)}=\sqrt{\sum_i>k D^2_i}
+    assert( torch.norm(M-U@torch.diag(S)@V.t())-torch.sqrt(torch.sum(S0[k:]**2)) 
+        < S0[0]*(m**2)*1e-14 )
+
+if __name__=='__main__':
+    test_SVDSYMARNOLDI_random()
+    test_SVDARNOLDI_random()
