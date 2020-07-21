bcc664f3d8619db63e0553514497610154ba70ea
Juraj Hasik <juraj.hasik.sk@gmail.com>
2020-07-21 14:37:03 +0200

    added params to ctmrg j1j2 c4v

--- examples/ctmrg_j1j2_c4v.py
+++ examples/ctmrg_j1j2_c4v.py
@@ -17,6 +17,8 @@ parser= cfg.get_args_parser()
 # additional model-dependent arguments
 parser.add_argument("--j1", type=float, default=1., help="nearest-neighbour coupling")
 parser.add_argument("--j2", type=float, default=0., help="next nearest-neighbour coupling")
+parser.add_argument("--hz_stag", type=float, default=0., help="staggered mag. field")
+parser.add_argument("--delta_zz", type=float, default=1., help="easy-axis (nearest-neighbour) anisotropy")
 # additional observables-related arguments
 parser.add_argument("--corrf_canonical", action='store_true', help="align spin operators" \
     + " with the vector of spontaneous magnetization")
@@ -37,7 +39,8 @@ def main():
     torch.set_num_threads(args.omp_cores)
     torch.manual_seed(args.seed)
     
-    model = j1j2.J1J2_C4V_BIPARTITE(j1=args.j1, j2=args.j2)
+    model= j1j2.J1J2_C4V_BIPARTITE(j1=args.j1, j2=args.j2, hz_stag=args.hz_stag, \
+        delta_zz=args.delta_zz)
     energy_f= model.energy_1x1_lowmem
     # energy_f= model.energy_1x1
 
