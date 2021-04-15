444da34e1e673723b21300b3aaeba6dd08b8e404
sdiop01 <diop.seydou1@gmail.com>
2021-04-15 17:00:39 +0200

    wip: optimization D=7

--- SU3_CSL_ipeps/SU3_CSL_ctmrg.py
+++ SU3_CSL_ipeps/SU3_CSL_ctmrg.py
@@ -15,6 +15,8 @@ import unittest
 import logging
 log = logging.getLogger(__name__)
  
+print(torch.cuda.is_available())
+print(torch.cuda.current_device())
 
 # parse command line args and build necessary configuration objects
 parser= cfg.get_args_parser()
