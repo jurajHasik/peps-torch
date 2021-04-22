6ea057060c26c76034e3cfbab37405d4ffdc3779
Juraj Hasik <juraj.hasik.sk@gmail.com>
2021-04-22 15:04:06 +0200

    product state boundary init for CTM

--- SU3_CSL_ipeps/SU3_CSL_ctmrg.py
+++ SU3_CSL_ipeps/SU3_CSL_ctmrg.py
@@ -24,53 +24,60 @@ args, unknown_args= parser.parse_known_args()
 
 def main():
 
-	cfg.configure(args)
-	cfg.print_config()
-	print('\n')
-	torch.set_num_threads(args.omp_cores)
-	torch.manual_seed(args.seed)
-	
-	# Import all elementary tensors and build initial state
-	elementary_tensors = []
-	for name in ['S0','S1','S2','S3','S4','L0','L1','L2']:
-		ts = load_SU3_tensor(name)
-		elementary_tensors.append(ts)
-	coeffs = {(0,0): torch.tensor([0.,0.,0.,0.,0.,0.],dtype=torch.complex128)}
-	state = IPEPS_U1SYM(elementary_tensors, coeffs)
-	state.add_noise(args.instate_noise)
-	
-	model = SU3_chiral.SU3_CHIRAL(theta = args.theta)
-	
-	def energy_f(state, env):
-		return model.energy_triangle(state,env)
-	
-	def ctmrg_conv_energy(state, env, history, ctm_args=cfg.ctm_args):
-		if not history:
-			history=[]
-		e_curr= energy_f(state,env)
-		history.append(e_curr.item())
-		print('Step n°'+str(len(history))+'     E_site = '+str(e_curr.item()))
-		if (len(history) > 1 and abs(history[-1]-history[-2]) < ctm_args.ctm_conv_tol)\
-			or len(history) >= ctm_args.ctm_max_iter:
-			log.info({"history_length": len(history), "history": history})
-			return True, history
-		return False, history
+    cfg.configure(args)
+    cfg.print_config()
+    print('\n')
+    torch.set_num_threads(args.omp_cores)
+    torch.manual_seed(args.seed)
+    
+    # Import all elementary tensors and build initial state
+    elementary_tensors = []
+    for name in ['S0','S1','S2','S3','S4','L0','L1','L2']:
+        ts = load_SU3_tensor(name)
+        elementary_tensors.append(ts)
+    coeffs = {(0,0): torch.tensor([0.,0.,0.,0.,0.,0.],dtype=torch.complex128)}
+    state = IPEPS_U1SYM(elementary_tensors, coeffs)
+    state.add_noise(args.instate_noise)
+    
+    model = SU3_chiral.SU3_CHIRAL(theta = args.theta)
+    
+    def energy_f(state, env):
+        return model.energy_triangle(state,env)
+    
+    def ctmrg_conv_energy(state, env, history, ctm_args=cfg.ctm_args):
+        if not history:
+            history=[]
+        e_curr= energy_f(state,env)
+        history.append(e_curr.item())
+        print('Step n°'+str(len(history))+'     E_site = '+str(e_curr.item()))
+        
+        for c_loc,c_ten in env.C.items(): 
+            u,s,v= torch.svd(c_ten, compute_uv=False)
+            print(f"\n\nspectrum C[{c_loc}]")
+            for i in range(args.chi):
+                print(f"{i} {s[i]}")
 
-	ctm_env_init = ENV(args.chi, state)
-	init_env(state, ctm_env_init)
-	
-	e_dn_init = energy_f(state, ctm_env_init)
-	print('*** Energy per site (before CTMRG) -- down triangles: '+str(e_dn_init.item()))
-	
-	ctm_env_out, *ctm_log= ctmrg.run(state, ctm_env_init, conv_check=ctmrg_conv_energy)
-	
-	e_dn_final = energy_f(state,ctm_env_out)
-	print('*** Energy per site (after CTMRG) -- down triangles: '+str(e_dn_final.item()))
+        if (len(history) > 1 and abs(history[-1]-history[-2]) < ctm_args.ctm_conv_tol)\
+            or len(history) >= ctm_args.ctm_max_iter:
+            log.info({"history_length": len(history), "history": history})
+            return True, history
+        return False, history
 
-	
+    ctm_env_init = ENV(args.chi, state)
+    init_env(state, ctm_env_init)
+    
+    e_dn_init = energy_f(state, ctm_env_init)
+    print('*** Energy per site (before CTMRG) -- down triangles: '+str(e_dn_init.item()))
+    
+    ctm_env_out, *ctm_log= ctmrg.run(state, ctm_env_init, conv_check=ctmrg_conv_energy)
+    
+    e_dn_final = energy_f(state,ctm_env_out)
+    print('*** Energy per site (after CTMRG) -- down triangles: '+str(e_dn_final.item()))
+
+    
 if __name__=='__main__':
-	if len(unknown_args)>0:
-		print("args not recognized: "+str(unknown_args))
-		raise Exception("Unknown command line arguments")
-	main()
+    if len(unknown_args)>0:
+        print("args not recognized: "+str(unknown_args))
+        raise Exception("Unknown command line arguments")
+    main()
 
