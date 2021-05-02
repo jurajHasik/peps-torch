0cd4acc16ebaae81e79de5ec22e12c88f726dd21
sdiop01 <diop.seydou1@gmail.com>
2021-05-02 17:28:51 +0200

    wip: cross-checking Ji-Yao's results

--- SU3_CSL_ipeps/SU3_CSL_optim.py
+++ SU3_CSL_ipeps/SU3_CSL_optim.py
@@ -37,7 +37,7 @@ def main():
 		ts = load_SU3_tensor(name)
 		elementary_tensors.append(ts)
 	# define initial coefficients
-	coeffs = {(0,0): torch.tensor([0.146103,-0.418167,1.64509,1.14427, 0.277921, 0., 0., -0.1490097,-1.87683,0.],dtype=torch.float64)}
+	coeffs = {(0,0): torch.tensor([-0.418167,-0.1490097, -1.87683, 0.146103, 1.64509, 0., 0., 1.14427, 0.277921, 0.],dtype=torch.float64)}
 	# define which coefficients will be added a noise
 	var_coeffs_allowed = torch.tensor([1,1,1,1,1, 0,0, 1,1,0])
 	state = IPEPS_U1SYM(elementary_tensors, coeffs, var_coeffs_allowed)
