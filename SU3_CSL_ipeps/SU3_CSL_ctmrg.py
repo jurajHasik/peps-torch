33d244f039df5d3960e28477401465eac9d07d6b
sdiop01 <diop.seydou1@gmail.com>
2021-05-16 17:48:09 +0200

    wip: optimization D=7

--- SU3_CSL_ipeps/SU3_CSL_ctmrg.py
+++ SU3_CSL_ipeps/SU3_CSL_ctmrg.py
@@ -45,7 +45,7 @@ def main():
     # define initial coefficients
     #coeffs = {(0, 0): torch.tensor([1.0000,  0.3563,  4.4882, -0.3494, -3.9341, 0., 0., 1.0000, 0.2429, 0.], dtype=torch.float64)}
     #coeffs = {(0,0): torch.tensor([1.,0.,0.,0.,0.,0.,0.,1.,0.,0.], dtype=torch.float64)}
-    coeffs = {(0, 0): torch.tensor([1.0000, -0.8700,  1.5465,  0.0000,  0.0000,  0.0000,  0.0000,  1.0000,
+    coeffs = {(0, 0): torch.tensor([1.0000, -0.8699,  1.5465,  0.0000,  0.0000,  0.0000,  0.0000,  1.0000,
          1.4435,  0.0000], dtype=torch.float64)}
     # define which coefficients will be added a noise
     var_coeffs_allowed = torch.tensor([0, 1, 1, 1, 1, 0, 0, 0, 1, 0], dtype=torch.float64)
