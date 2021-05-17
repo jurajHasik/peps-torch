1293fb0e43870bfc197ed4970bfdd1e998aee745
sdiop01 <diop.seydou1@gmail.com>
2021-05-17 12:31:46 +0200

    wip: adapting for GPU

--- SU3_CSL_ipeps/SU3_CSL_ctmrg.py
+++ SU3_CSL_ipeps/SU3_CSL_ctmrg.py
@@ -46,7 +46,7 @@ def main():
     #coeffs = {(0, 0): torch.tensor([1.0000,  0.3563,  4.4882, -0.3494, -3.9341, 0., 0., 1.0000, 0.2429, 0.], dtype=torch.float64)}
     #coeffs = {(0,0): torch.tensor([1.,0.,0.,0.,0.,0.,0.,1.,0.,0.], dtype=torch.float64)}
     coeffs = {(0, 0): torch.tensor([1.0000, -0.8699,  1.5465,  0.0000,  0.0000,  0.0000,  0.0000,  1.0000,
-         1.4435,  0.0000], dtype=torch.float64)}
+         1.4435,  0.0000], dtype=torch.float64, device=args.GLOBALARGS_device)}
     # define which coefficients will be added a noise
     var_coeffs_allowed = torch.tensor([0, 1, 1, 1, 1, 0, 0, 0, 1, 0], dtype=torch.float64, device=args.GLOBALARGS_device)
     #var_coeffs_allowed = torch.tensor([0, 0, 0, 0, 0, 1, 1, 1, 1, 1], dtype=torch.float64)
