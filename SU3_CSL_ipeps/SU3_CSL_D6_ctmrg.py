d1e0d7634cdd22f8bb3903d25406e45d6d931fc2
sdiop01 <diop.seydou1@gmail.com>
2021-06-04 14:59:03 +0200

    D=9 tensors

--- SU3_CSL_ipeps/SU3_CSL_D6_ctmrg.py
+++ SU3_CSL_ipeps/SU3_CSL_D6_ctmrg.py
@@ -64,12 +64,12 @@ def main():
         # coeffs ... .to(t_device)
     else:
         # AKLT state
-        coeffs_triangle = {(0, 0): torch.tensor([1., 0., 0., 0., 0., 0.], dtype=torch.float64, device=t_device)}
+        coeffs_triangle = {(0, 0): torch.tensor([0., 0., 0., 0., 0., 0.], dtype=torch.float64, device=t_device)}
         coeffs_site = {(0, 0): torch.tensor([1., 0., 0., 0.], dtype=torch.float64, device=t_device)}
 
     # define which coefficients will be added a noise
     var_coeffs_site = torch.tensor([0, 1, 1, 1], dtype=torch.float64, device=t_device)
-    var_coeffs_triangle = torch.tensor([0, 1, 1, 1, 1, 1], dtype=torch.float64, device=t_device)
+    var_coeffs_triangle = torch.tensor([0, 0, 0, 0, 1, 1], dtype=torch.float64, device=t_device)
 
     state = IPEPS_U1SYM(tensors_triangle, tensors_site, coeffs_triangle_up=coeffs_triangle, coeffs_site=coeffs_site,
                         sym_up_dn=bool(args.sym_up_dn),
