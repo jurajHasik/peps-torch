e0100afd92a62426e50a7f5fc7afaa3534ac39e0
Juraj Hasik <juraj.hasik.sk@gmail.com>
2021-06-19 12:54:59 +0200

    wip: including PGs

--- examples/optim_csl_kagome.py
+++ examples/optim_csl_kagome.py
@@ -5,7 +5,7 @@ import math
 import torch
 import copy
 from collections import OrderedDict
-from ipeps.ipeps_kagome import IPEPS_KAGOME, read_ipeps_kagome, extend_bond_dim
+from ipeps.ipeps_kagome import IPEPS_KAGOME, read_ipeps_kagome, extend_bond_dim, to_PG_symmetric
 from models import SU3_chiral
 from ctm.generic.env import *
 from ctm.generic import ctmrg
@@ -22,7 +22,8 @@ parser = cfg.get_args_parser()
 parser.add_argument("--theta", type=float, default=0, help="angle [<value> x pi] parametrizing the chiral Hamiltonian")
 parser.add_argument("--j1", type=float, default=0, help="nearest-neighbor exchange coupling")
 parser.add_argument("--j2", type=float, default=0, help="next-nearest-neighbor exchange coupling")
-parser.add_argument("--ansatz", type=str, default="A1+iA2, B", help="choice of the tensor ansatz")
+parser.add_argument("--ansatz", type=str, default=None, help="choice of the tensor ansatz",\
+     choices=[None, 'A_2,B'])
 parser.add_argument("--no_sym_up_dn", action='store_false', dest='sym_up_dn',\
     help="same trivalent tensors for up and down triangles")
 args, unknown_args = parser.parse_known_args()
@@ -38,8 +39,21 @@ def main():
         Ki=math.sin(args.theta * math.pi), j1=args.j1, j2=args.j2)
 
     # initialize the ipeps
+    if args.ansatz=="A_2,B":
+        ansatz_pgs= ("A_2", "A_2", "B")
+    elif args.ansatz== None or args.ansatz== "":
+        ansatz_pgs= None
     if args.instate!=None:
         state= read_ipeps_kagome(args.instate)
+
+        # possibly symmetrize by PG
+        if ansatz_pgs!=None and state.pgs==(None,None,None):
+            state= to_PG_symmetric(state, ansatz_pgs)
+        elif ansatz_pgs!=None and state.pgs==ansatz_pgs:
+            pass
+        elif ansatz_pgs!=None and state.pgs!=ansatz_pgs:
+            raise RuntimeError("instate has incompatible PG symmetry with "+args.ansatz)
+
         if args.bond_dim > max(state.get_aux_bond_dims()):
             # extend the auxiliary dimensions
             state= extend_bond_dim(state, args.bond_dim)
@@ -51,7 +65,7 @@ def main():
             args.bond_dim, dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0)
         B_S= torch.zeros(3, args.bond_dim, args.bond_dim,\
             dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)
-        state= IPEPS_KAGOME(T_U, B_S, T_D, SYM_UP_DOWN=args.sym_up_dn)
+        state= IPEPS_KAGOME(T_U, B_S, T_D, SYM_UP_DOWN=args.sym_up_dn, pgs=ansatz_pgs)
         state.load_checkpoint(args.opt_resume)
     elif args.ipeps_init_type=='RANDOM':
         bond_dim = args.bond_dim
@@ -61,7 +75,7 @@ def main():
             dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0)
         B_S= torch.rand(3, bond_dim, bond_dim,\
             dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0
-        state = IPEPS_KAGOME(T_U, B_S, T_D, SYM_UP_DOWN=args.sym_up_dn)
+        state = IPEPS_KAGOME(T_U, B_S, T_D, SYM_UP_DOWN=args.sym_up_dn, pgs=ansatz_pgs)
     else:
         raise ValueError("Missing trial state: -instate=None and -ipeps_init_type= "\
             +str(args.ipeps_init_type)+" is not supported")
@@ -89,7 +103,6 @@ def main():
         print(f"\n\nspectrum C[{spectra[0][0]}]             spectrum C[{spectra[1][0]}]             spectrum C[{spectra[2][0]}]             spectrum C[{spectra[3][0]}] ")
         for i in range(args.chi):
             print("{:2} {:01.14f}        {:2} {:01.14f}        {:2} {:01.14f}        {:2} {:01.14f}".format(i, spectra[0][1][i], i, spectra[1][1][i], i, spectra[2][1][i], i, spectra[3][1][i]))
-
 
     @torch.no_grad()
     def ctmrg_conv_energy(state, env, history, ctm_args=cfg.ctm_args):
@@ -129,15 +142,23 @@ def main():
     def loss_fn(state, ctm_env_in, opt_context):
         ctm_args = opt_context["ctm_args"]
         opt_args = opt_context["opt_args"]
+
         # build on-site tensors
-        state.sites = state.build_onsite_tensors()
+        if state.pgs!=(None,None,None):
+            # explicit symmetrize (which rebuilds the on-site tensor)
+            state_sym= to_PG_symmetric(state, state.pgs)
+        else:
+            # explicit rebuild of on-site tensors
+            state_sym= state
+            state_sym.sites= state_sym.build_onsite_tensors()
+
         # possibly re-initialize the environment
         if opt_args.opt_ctm_reinit:
-            init_env(state, ctm_env_in)
+            init_env(state_sym, ctm_env_in)
         # compute environment by CTMRG
-        ctm_env_out, history, t_ctm, t_conv_check = ctmrg.run(state, ctm_env_in, \
+        ctm_env_out, history, t_ctm, t_conv_check = ctmrg.run(state_sym, ctm_env_in, \
             conv_check=ctmrg_conv_energy, ctm_args=ctm_args)
-        loss0 = energy_f(state, ctm_env_out, force_cpu=cfg.ctm_args.conv_check_cpu)
+        loss0 = energy_f(state_sym, ctm_env_out, force_cpu=cfg.ctm_args.conv_check_cpu)
 
         # loc_ctm_args = copy.deepcopy(ctm_args)
         # loc_ctm_args.ctm_max_iter = 1
@@ -150,6 +171,7 @@ def main():
 
     @torch.no_grad()
     def obs_fn(state, ctm_env, opt_context):
+        state_sym= to_PG_symmetric(state, state.pgs)
         if opt_context["line_search"]:
             epoch= len(opt_context["loss_history"]["loss_ls"])
             loss= opt_context["loss_history"]["loss_ls"][-1]
@@ -157,7 +179,7 @@ def main():
         else:
             epoch= len(opt_context["loss_history"]["loss"]) 
             loss= opt_context["loss_history"]["loss"][-1] 
-        obs_values, obs_labels = model.eval_obs(state,ctm_env,force_cpu=False)
+        obs_values, obs_labels = model.eval_obs(state_sym,ctm_env,force_cpu=False)
         print(", ".join([f"{epoch}",f"{loss}"]+[f"{v}" for v in obs_values]))
 
     optimize_state(state, ctm_env_init, loss_fn, obs_fn=obs_fn)
