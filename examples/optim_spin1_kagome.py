c2c18003ec8796466d5035c6cb5d158f7dd61aa6
Juraj Hasik <juraj.hasik.sk@gmail.com>
2021-09-14 22:33:27 +0200

    wip: spin1

---
+++ examples/optim_spin1_kagome.py
@@ -0,0 +1,167 @@
+import context
+import argparse
+import config as cfg
+import math
+import torch
+import copy
+from collections import OrderedDict
+from ipeps.ipess_kagome import IPESS_KAGOME, read_ipess_kagome, extend_bond_dim, to_PG_symmetric
+from models import spin1_kagome
+from ctm.generic.env import *
+from ctm.generic import ctmrg
+from ctm.generic import rdm
+from optim.ad_optim_lbfgs_mod import optimize_state
+import json
+import unittest
+import logging
+
+log = logging.getLogger(__name__)
+
+# parse command line args and build necessary configuration objects
+parser = cfg.get_args_parser()
+parser.add_argument("--theta", type=float, default=0, help="angle [<value> x pi] parametrizing the chiral Hamiltonian")
+parser.add_argument("--j1", type=float, default=1., help="nearest-neighbor exchange coupling")
+parser.add_argument("--j1sq", type=float, default=0, help="nearest-neighbor biquadratic exchange coupling")
+parser.add_argument("--j2", type=float, default=0, help="next-nearest-neighbor exchange coupling")
+parser.add_argument("--j2sq", type=float, default=0, help="next-nearest-neighbor biquadratic exchange coupling")
+parser.add_argument("--jtrip", type=float, default=0, help="(SxS).S")
+parser.add_argument("--jperm", type=float, default=0, help="triangle permutation")
+parser.add_argument("--ansatz", type=str, default=None, help="choice of the tensor ansatz",\
+     choices=[None, 'A_2,B'])
+parser.add_argument("--no_sym_up_dn", action='store_false', dest='sym_up_dn',\
+    help="same trivalent tensors for up and down triangles")
+args, unknown_args = parser.parse_known_args()
+
+
+def main():
+    cfg.configure(args)
+    cfg.print_config()
+    torch.set_num_threads(args.omp_cores)
+    torch.manual_seed(args.seed)
+
+    model= spin1_kagome.S1_KAGOME(j1=args.j1,j1sq=args.j1sq,j2=args.j2,j2sq=args.j2sq,\
+        jtrip=args.jtrip,jperm=args.jperm)
+
+    # initialize the ipeps
+    if args.ansatz=="A_2,B":
+        ansatz_pgs= ("A_2", "A_2", "B")
+    elif args.ansatz== None or args.ansatz== "":
+        ansatz_pgs= None
+    if args.instate!=None:
+        state= read_ipess_kagome(args.instate)
+
+        # possibly symmetrize by PG
+        if ansatz_pgs!=None and state.pgs==(None,None,None):
+            state= to_PG_symmetric(state, ansatz_pgs)
+        elif ansatz_pgs!=None and state.pgs==ansatz_pgs:
+            pass
+        elif ansatz_pgs!=None and state.pgs!=ansatz_pgs:
+            raise RuntimeError("instate has incompatible PG symmetry with "+args.ansatz)
+
+        if args.bond_dim > max(state.get_aux_bond_dims()):
+            # extend the auxiliary dimensions
+            state= extend_bond_dim(state, args.bond_dim)
+        state.add_noise(args.instate_noise)
+    elif args.opt_resume is not None:
+        T_U= torch.zeros(args.bond_dim, args.bond_dim, args.bond_dim,\
+            dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)
+        T_D= None if args.sym_up_dn else (torch.zeros(args.bond_dim, args.bond_dim,\
+            args.bond_dim, dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0)
+        B_S= torch.zeros(3, args.bond_dim, args.bond_dim,\
+            dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)
+        state= IPESS_KAGOME(T_U, B_S, T_D, SYM_UP_DOWN=args.sym_up_dn, pgs=ansatz_pgs)
+        state.load_checkpoint(args.opt_resume)
+    elif args.ipeps_init_type=='RANDOM':
+        bond_dim = args.bond_dim
+        T_U= torch.rand(bond_dim, bond_dim, bond_dim,\
+            dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0
+        T_D= None if args.sym_up_dn else (torch.rand(bond_dim, bond_dim, bond_dim,\
+            dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0)
+        B_S= torch.rand(3, bond_dim, bond_dim,\
+            dtype=cfg.global_args.torch_dtype, device=cfg.global_args.device)-1.0
+        state = IPESS_KAGOME(T_U, B_S, T_D, SYM_UP_DOWN=args.sym_up_dn, pgs=ansatz_pgs)
+    else:
+        raise ValueError("Missing trial state: -instate=None and -ipeps_init_type= "\
+            +str(args.ipeps_init_type)+" is not supported")
+
+
+    def energy_f(state, env, force_cpu=False):
+        e_dn = model.energy_triangle_dn(state, env, force_cpu=force_cpu)
+        e_up = model.energy_triangle_up(state, env, force_cpu=force_cpu)
+        # e_nnn = model.energy_nnn(state, env)
+        return (e_up + e_dn)/3 #+ e_nnn) / 3
+
+    @torch.no_grad()
+    def ctmrg_conv_energy(state, env, history, ctm_args=cfg.ctm_args):
+        if not history:
+            history = []
+        e_curr = energy_f(state, env, force_cpu=ctm_args.conv_check_cpu)
+        history.append(e_curr.item())
+        if (len(history) > 1 and abs(history[-1] - history[-2]) < ctm_args.ctm_conv_tol) \
+                or len(history) >= ctm_args.ctm_max_iter:
+            log.info({"history_length": len(history), "history": history})
+            return True, history
+        return False, history
+
+    ctm_env_init = ENV(args.chi, state)
+    init_env(state, ctm_env_init)
+
+    ctm_env_init, history, t_ctm, t_conv_check = ctmrg.run(state, ctm_env_init, \
+            conv_check=ctmrg_conv_energy, ctm_args=cfg.ctm_args)
+    
+    loss0 = energy_f(state, ctm_env_init, force_cpu=cfg.ctm_args.conv_check_cpu)
+    obs_values, obs_labels = model.eval_obs(state,ctm_env_init,force_cpu=False)
+    print("\n\n",end="")
+    print(", ".join(["epoch",f"loss"]+[label for label in obs_labels]))
+    print(", ".join([f"{-1}",f"{loss0}"]+[f"{v}" for v in obs_values]))
+
+    def loss_fn(state, ctm_env_in, opt_context):
+        ctm_args = opt_context["ctm_args"]
+        opt_args = opt_context["opt_args"]
+
+        # build on-site tensors
+        if state.pgs!=(None,None,None):
+            # explicit symmetrize (which rebuilds the on-site tensor)
+            state_sym= to_PG_symmetric(state, state.pgs)
+        else:
+            # explicit rebuild of on-site tensors
+            state_sym= state
+            state_sym.sites= state_sym.build_onsite_tensors()
+
+        # possibly re-initialize the environment
+        if opt_args.opt_ctm_reinit:
+            init_env(state_sym, ctm_env_in)
+        # compute environment by CTMRG
+        ctm_env_out, history, t_ctm, t_conv_check = ctmrg.run(state_sym, ctm_env_in, \
+            conv_check=ctmrg_conv_energy, ctm_args=ctm_args)
+        loss0 = energy_f(state_sym, ctm_env_out, force_cpu=cfg.ctm_args.conv_check_cpu)
+
+        # loc_ctm_args = copy.deepcopy(ctm_args)
+        # loc_ctm_args.ctm_max_iter = 1
+        # ctm_env_out, history1, t_ctm1, t_obs1 = ctmrg.run(state, ctm_env_out, ctm_args=loc_ctm_args)
+        # loss1 = energy_f(state, ctm_env_out, force_cpu=cfg.ctm_args.conv_check_cpu)
+        # loss = torch.max(loss0, loss1)
+        
+        loss= loss0
+        return loss, ctm_env_out, history, t_ctm, t_conv_check
+
+    @torch.no_grad()
+    def obs_fn(state, ctm_env, opt_context):
+        state_sym= to_PG_symmetric(state, state.pgs)
+        if opt_context["line_search"]:
+            epoch= len(opt_context["loss_history"]["loss_ls"])
+            loss= opt_context["loss_history"]["loss_ls"][-1]
+            print("LS",end=" ")
+        else:
+            epoch= len(opt_context["loss_history"]["loss"]) 
+            loss= opt_context["loss_history"]["loss"][-1] 
+        obs_values, obs_labels = model.eval_obs(state_sym,ctm_env,force_cpu=False)
+        print(", ".join([f"{epoch}",f"{loss}"]+[f"{v}" for v in obs_values]))
+
+    optimize_state(state, ctm_env_init, loss_fn, obs_fn=obs_fn)
+
+if __name__ == '__main__':
+    if len(unknown_args) > 0:
+        print("args not recognized: " + str(unknown_args))
+        raise Exception("Unknown command line arguments")
+    main()
---
+++ models/spin1_kagome.py
@@ -0,0 +1,214 @@
+import torch
+import config as cfg
+from ctm.generic.env import ENV
+from ctm.generic import rdm
+from ctm.pess_kagome import rdm_kagome
+from ctm.generic import corrf
+import groups.su2 as su2
+from math import sqrt
+from numpy import exp
+import itertools
+
+def _cast_to_real(t, check=True, imag_eps=1.0e-12):
+    if t.is_complex():
+        assert abs(t.imag) < imag_eps,"unexpected imaginary part "+str(t.imag)
+        return t.real
+    return t
+
+class S1_KAGOME():
+
+    def __init__(self, j1=0., j1sq=0., j2=0., j2sq=0., jtrip=0., jperm=0., global_args=cfg.global_args):
+        r"""
+        H = J_1 \sum_{<ij>} S_i.S_j + J_{1sq} \sum_{<ij>} (S_i.S_j)^2
+            + J_2 \sum_{<<ij>>} S_i.S_j + J_{2sq} \sum_{<<ij>>} (S_i.S_j)^2
+            - J_{trip} \sum_t (S_{t_1} \times S_{t_2}).S_{t_3}
+            + J_{perm} \sum_t P_t + J*_{perm} \sum_t P^{-1}_t
+        """
+        self.dtype = global_args.torch_dtype
+        self.device = global_args.device
+        self.phys_dim = 3
+        self.j1 = j1
+        self.j1sq = j1sq
+        self.j2 = j2
+        self.j2sq = j2sq
+        self.jtrip = jtrip
+        self.jperm = jperm
+        
+        irrep = su2.SU2(self.phys_dim, dtype=self.dtype, device=self.device)
+
+        Id1= irrep.I()
+        self.Id3_t= torch.eye(self.phys_dim**3, dtype=self.dtype, device=self.device)
+        SS= irrep.SS()
+        SSnnId= torch.einsum('ijkl,ab->ijaklb',SS,Id1)
+        SSnn_t= SSnnId + SSnnId.permute(1,2,0, 4,5,3) + SSnnId.permute(2,0,1, 5,3,4)
+        SS2= torch.einsum('ijab,abkl->ijkl',SS,SS)
+        SS2nnId= torch.einsum('ijkl,ab->ijaklb',SS2,Id1)
+        SS2nn_t= SS2nnId + SS2nnId.permute(1,2,0, 4,5,3) + SS2nnId.permute(2,0,1, 5,3,4)
+
+        Svec= irrep.S()
+        levicivit3= torch.zeros(3,3,3, dtype=self.dtype, device=self.device)
+        levicivit3[0,1,2]=levicivit3[1,2,0]=levicivit3[2,0,1]=1.
+        levicivit3[0,2,1]=levicivit3[2,1,0]=levicivit3[1,0,2]=-1.
+        SxSS_t= torch.einsum('abc,bij,ckl,amn->ikmjln',levicivit3,Svec,Svec,Svec)
+
+        permute_triangle = torch.zeros([3]*6, dtype=self.dtype, device=self.device)
+        permute_triangle_inv = torch.zeros([3]*6, dtype=self.dtype, device=self.device)
+        for i in range(self.phys_dim):
+            for j in range(self.phys_dim):
+                for k in range(self.phys_dim):
+                    # anticlockwise (direct)
+                    permute_triangle[i, j, k, j, k, i] = 1.
+                    # clockwise (inverse)
+                    permute_triangle_inv[i, j, k, k, i, j] = 1.
+
+        self.h_triangle= self.j1*SSnn_t + self.j1sq*SS2nn_t + self.jtrip*SxSS_t \
+            + self.jperm * permute_triangle + (self.jperm * permute_triangle_inv).conj()
+        szId2= torch.einsum('ij,kl,ab->ikajlb',irrep.SZ(),Id1,Id1).contiguous()
+        spId2= torch.einsum('ij,kl,ab->ikajlb',irrep.SP(),Id1,Id1).contiguous()
+        smId2= torch.einsum('ij,kl,ab->ikajlb',irrep.SM(),Id1,Id1).contiguous()
+        self.obs_ops= {
+            "sz_0": szId2, "sp_0": spId2, "sm_0": smId2,\
+            "sz_1": szId2.permute(1,2,0, 4,5,3).contiguous(),\
+            "sp_1": spId2.permute(1,2,0, 4,5,3).contiguous(),\
+            "sm_1": smId2.permute(1,2,0, 4,5,3).contiguous(),\
+            "sz_2": szId2.permute(2,0,1, 5,3,4).contiguous(),\
+            "sp_2": spId2.permute(2,0,1, 5,3,4).contiguous(),\
+            "sm_2": smId2.permute(2,0,1, 5,3,4).contiguous(),
+            }
+
+
+    # Energy terms
+    def energy_triangle_dn(self, state, env, force_cpu=False):
+        e_dn= rdm_kagome.rdm2x2_dn_triangle_with_operator(\
+            (0, 0), state, env, self.h_triangle, force_cpu=force_cpu)
+        return _cast_to_real(e_dn)
+
+    def energy_triangle_up(self, state, env, force_cpu=False):
+        rdm_up= rdm_kagome.rdm2x2_up_triangle_open(\
+            (0, 0), state, env, force_cpu=force_cpu)
+        e_up= torch.einsum('ijkmno,mnoijk', rdm_up, self.h_triangle )
+        return _cast_to_real(e_up)
+
+    def energy_nnn(self, state, env, force_cpu=False):
+        if self.j2 == 0:
+            return 0.
+        else:
+            vNNN = self.P_bonds_nnn(state, env, force_cpu=force_cpu)
+            return(self.j2*(vNNN[0]+vNNN[1]+vNNN[2]+vNNN[3]+vNNN[4]+vNNN[5]))
+
+    # Observables
+
+    def P_dn(self, state, env, force_cpu=False):
+        vP_dn= rdm_kagome.rdm2x2_dn_triangle_with_operator((0, 0), state, env,\
+            operator=permute_triangle, force_cpu=force_cpu)
+        return vP_dn
+
+    def P_up(self, state, env, force_cpu=False):
+        rdm_up= rdm_kagome.rdm2x2_up_triangle_open((0, 0), state, env, force_cpu=force_cpu)
+        vP_up= torch.einsum('ijkmno,mnoijk', rdm_up, permute_triangle)
+        return vP_up
+
+    def P_bonds_nnn(self, state, env, force_cpu=False):
+        norm_wf = rdm_kagome.rdm2x2_dn_triangle_with_operator((0, 0), state, env, \
+            self.id_downT, force_cpu=force_cpu)
+        vNNN1_12, vNNN1_31 = rdm_kagome.rdm2x2_nnn_1((0, 0), state, env, operator=exchange_bond, force_cpu=force_cpu)
+        vNNN2_32, vNNN2_21 = rdm_kagome.rdm2x2_nnn_2((0, 0), state, env, operator=exchange_bond, force_cpu=force_cpu)
+        vNNN3_31, vNNN3_23 = rdm_kagome.rdm2x2_nnn_3((0, 0), state, env, operator=exchange_bond, force_cpu=force_cpu)
+        return _cast_to_real(vNNN1_12 / norm_wf), _cast_to_real(vNNN2_21 / norm_wf), \
+            _cast_to_real(vNNN1_31 / norm_wf), _cast_to_real(vNNN3_31 / norm_wf), \
+            _cast_to_real(vNNN2_32 / norm_wf), _cast_to_real(vNNN3_23 / norm_wf)
+
+    def P_bonds_nn(self, state, env):
+        id_matrix = torch.eye(27, dtype=torch.complex128, device=cfg.global_args.device)
+        norm_wf = rdm.rdm1x1((0, 0), state, env, operator=id_matrix)
+        # bond 2--3
+        bond_op = torch.zeros((27, 27), dtype=torch.complex128, device=cfg.global_args.device)
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    bond_op[fmap(i,j,k),fmap(i,k,j)] = 1.
+        vP_23 = rdm.rdm1x1((0,0), state, env, operator=bond_op) / norm_wf
+        # bond 1--3
+        bond_op = torch.zeros((27, 27), dtype=torch.complex128, device=cfg.global_args.device)
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    bond_op[fmap(i,j,k),fmap(k,j,i)] = 1.
+        vP_13 = rdm.rdm1x1((0, 0), state, env, operator=bond_op) / norm_wf
+        # bond 1--2
+        bond_op = torch.zeros((27, 27), dtype=torch.complex128, device=cfg.global_args.device)
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    bond_op[fmap(i,j,k),fmap(j,i,k)] = 1.
+        vP_12 = rdm.rdm1x1((0, 0), state, env, operator=bond_op) / norm_wf
+        return(torch.real(vP_23), torch.real(vP_13), torch.real(vP_12))
+
+    def eval_lambdas(self, state, env):
+        # computes the expectation value of the SU(3) observables \lambda_3 and \lambda_8 for the three sites of the unit cell
+        id_matrix = torch.eye(27, dtype=torch.complex128, device=cfg.global_args.device)
+        norm_wf = rdm.rdm1x1((0, 0), state, env, operator=id_matrix)
+        color3_1 = rdm.rdm1x1((0, 0), state, env, operator=lambda_3_1) / norm_wf
+        color3_2 = rdm.rdm1x1((0, 0), state, env, operator=lambda_3_2) / norm_wf
+        color3_3 = rdm.rdm1x1((0, 0), state, env, operator=lambda_3_3) / norm_wf
+        color8_1 = rdm.rdm1x1((0, 0), state, env, operator=lambda_8_1) / norm_wf
+        color8_2 = rdm.rdm1x1((0, 0), state, env, operator=lambda_8_2) / norm_wf
+        color8_3 = rdm.rdm1x1((0, 0), state, env, operator=lambda_8_3) / norm_wf
+        return (color3_1, color3_2, color3_3), (color8_1, color8_2, color8_3)
+
+    def eval_su3_gens(self, state, env):
+        id_matrix2 = torch.eye(9, dtype=torch.complex128, device=cfg.global_args.device)
+        id_matrix2 = id_matrix2.reshape(3,3,3,3)
+        id_matrix = torch.eye(27, dtype=torch.complex128, device=cfg.global_args.device)
+        norm_wf = rdm.rdm1x1((0, 0), state, env, operator=id_matrix)
+        # site 0
+        l8_ops_0= torch.einsum('ijx,klmn->ikmjlnx', self.su3_gens, id_matrix2).contiguous()
+        l8_ops_0= l8_ops_0.reshape(27,27,8)
+        for x in range(8):
+            l8_x_1x1= rdm.rdm1x1((0, 0), state, env, operator=l8_ops_0[:,:,x]) / norm_wf
+            print(f"{x} {l8_x_1x1}")
+
+        # site 1
+        l8_ops_1= torch.einsum('ijx,klmn->kimljnx', self.su3_gens, id_matrix2).contiguous()
+        l8_ops_1= l8_ops_1.reshape(27,27,8)
+        for x in range(8):
+            l8_x_1x1= rdm.rdm1x1((0, 0), state, env, operator=l8_ops_1[:,:,x]) / norm_wf
+            print(f"{x} {l8_x_1x1}")
+
+        # site 2
+        l8_ops_2= torch.einsum('ijx,klmn->kmilnjx', self.su3_gens, id_matrix2).contiguous()
+        l8_ops_2= l8_ops_2.reshape(27,27,8)
+        for x in range(8):
+            l8_x_1x1= rdm.rdm1x1((0, 0), state, env, operator=l8_ops_2[:,:,x]) / norm_wf
+            print(f"{x} {l8_x_1x1}")
+
+    def eval_obs(self,state,env,force_cpu=True):
+        r"""
+        :param state: wavefunction
+        :param env: CTM environment
+        :type state: IPEPS
+        :type env: ENV
+        :return:  expectation values of observables, labels of observables
+        :rtype: list[float], list[str]
+        """
+        norm_wf = rdm.rdm1x1((0, 0), state, env, operator=self.Id3_t)
+        obs= {"e_t_dn": 0, "e_t_up": 0, "m_0": 0, "m_1": 0, "m_2": 0}
+        with torch.no_grad():
+            e_t_dn= self.energy_triangle_dn(state, env, force_cpu=force_cpu)
+            e_t_up= self.energy_triangle_up(state, env, force_cpu=force_cpu)
+            obs["e_t_dn"]= e_t_dn
+            obs["e_t_up"]= e_t_up
+
+            for label in self.obs_ops.keys():
+                op= self.obs_ops[label].view(self.phys_dim**3, self.phys_dim**3)
+                obs_val= rdm.rdm1x1((0, 0), state, env, operator=op) / norm_wf
+                obs[f"{label}"]= obs_val #_cast_to_real(obs_val)
+
+            for i in range(3):
+                obs[f"m_{i}"]= sqrt(_cast_to_real(
+                    obs[f"sz_{i}"]*obs[f"sz_{i}"]\
+                    + obs[f"sp_{i}"].conj()*obs[f"sm_{i}"])
+                )
+
+        # prepare list with labels and values
+        return list(obs.values()), list(obs.keys())
